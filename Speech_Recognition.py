2420b9add7ba658b978cb63b0c3b807e136d9657
Binit Jha <52690097+Binit98@users.noreply.github.com>
2019-07-17 13:12:55 +0530

    Add files via upload

---
+++ Speech_Recognition.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 12:00:00 2019
+
+@author: BIN33T
+"""
+
+import speech_recognition as sr
+AUDIO_FILE=("binit_sample.wav")
+
+r=sr.Recognizer()
+
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio=r.record(source)
+    
+try:
+    print ("audio file contains " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print ("Google Speech Recognition could not understand audio")
+except sr.RequestError:
+    print ("Couldn't get  the results from Google Speech Recognition") 
